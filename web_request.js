303a90b608faeffa73a823491ddcd84fba4389d2
BuildTools <mathieu@usp-3.fr>
2020-03-03 04:35:41 +0100

    (for web)

---
+++ web_request.js
@@ -0,0 +1,14 @@
+module.exports = (url, json = {}, cb = () => null) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      let body = xhr.responseText;
+      if (!body || !body.success || body.success !== true) {
+        throw new Error("Error ! This server isn't compatible with (this version of) Blast...")
+      }else cb(body)
+    }
+  };
+  xhr.send(JSON.stringify(json));
+}
